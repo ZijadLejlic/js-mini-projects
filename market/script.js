c446a3ad9b61d756d1992eeb99fd717a10edb75e
yozi97 <lejliczijad@gmail.com>
2026-02-16 15:58:25 +0100

    practice

--- market/script.js
+++ market/script.js
@@ -1,1 +1,13 @@
-alert('Fajl se ucitao');
+function pokreniMe(dugme) {
+
+    let nazivDugmeta = dugme.innerText;
+    let klasaDugmeta = dugme.className;
+    console.log(klasaDugmeta);
+
+    let message = document.querySelector('#kliknutoDugme');
+    message.innerText = `Kliknuli ste na dugme: ${nazivDugmeta}`;
+
+    message.removeAttribute('class');
+
+
+}
