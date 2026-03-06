480db60e72d34df084fbb5064634492f74bcee0b
yozi97 <lejliczijad@gmail.com>
2026-03-06 15:31:29 +0100

    practice

---
+++ ajax/script.js
@@ -0,0 +1,9 @@
+/* let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
+    }
+};
+xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts/1", true);
+xhttp.send(); */
+
